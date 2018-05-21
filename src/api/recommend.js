9fb4110a3f98670f5f7c3c6cd68b19ae147cdd2a
tiffany <2073476165@qq.com>
2018-05-21 14:54:52 +0800

    loading

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,10 +2,10 @@ import jsonp from 'common/js/jsonp'
 // 因为是利用jsonp去抓取数据。
 import {commonParams, options} from './config'
 // 导入公用参数commonParams，options。因为是export，而非export default，所以导入要加{}。
-// import axios from 'axios'
+import axios from 'axios'
 
 export function getRecommend() {
-  // 封装获取getRecommend的方法
+  // 封装获取轮播数据的方法
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   const data = Object.assign({}, commonParams, {
     // Object.assign()方法es6用于对象的合并。
@@ -17,4 +17,26 @@ export function getRecommend() {
   })
 
   return jsonp(url, data, options)
+}
+
+export function getDiscList() {
+  // 封装获取推荐歌单数据的方法
+  const url = '/api/getDiscList'
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: 'json' 
+  })
+
+  return axios.get(url, {
+  	params: data
+  }).then((res) => {
+  	return Promise.resolve(res.data)
+  })
 }
--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import router from './router'
 import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
-
+import VueLazyload from 'vue-lazyload'
 fastclick.attach(document.body)
 // 解决所有div点击延迟3秒的问题
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.gif')
+})
 
 /* eslint-disable no-new */
 new Vue({
